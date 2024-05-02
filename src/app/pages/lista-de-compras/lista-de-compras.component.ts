919dadbc75d75d7819b78f5a2656b862a9fe31dc
Jose Henrique Falsarella Lima Oliveira <joseh.oliveira0329@gmail.com>
2024-05-02 01:19:40 -0300

    feat - Implementação da API no componente de Lista

--- src/app/pages/lista-de-compras/lista-de-compras.component.ts
+++ src/app/pages/lista-de-compras/lista-de-compras.component.ts
@@ -1,27 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-lista-de-compras',
   templateUrl: './lista-de-compras.component.html',
   styleUrls: ['./lista-de-compras.component.scss']
 })
-export class ListaDeComprasComponent {
+export class ListaDeComprasComponent implements OnInit {
   listForm: FormGroup;
   shoppingList: { name: string; quantity: number; done: boolean; }[] = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private apiService: ApiService
+  ) {
     this.listForm = this.formBuilder.group({
       writeItem: ['', Validators.required],
       insertQty: ['', [Validators.required, Validators.min(1)]]
     });
   }
 
+  ngOnInit(): void {
+    this.apiService.getAllItems().subscribe((shoppingList) => {
+      this.shoppingList = shoppingList;
+    })
+  }
+
   addItem() {
     if (this.listForm.valid) {
       const itemName = this.listForm.get('writeItem')?.value;
       const itemQty = this.listForm.get('insertQty')?.value;
-      this.shoppingList.push({ name: itemName, quantity: itemQty, done: false });
+
+      this.apiService.createItem(itemName, itemQty).subscribe((result: any) => {
+        console.log(result);
+        this.shoppingList.push({ name: itemName, quantity: itemQty, done: false });
+      })
+
       this.listForm.reset();
     } else {
       this.listForm.markAllAsTouched();
@@ -29,11 +43,17 @@ export class ListaDeComprasComponent {
   }
 
   removeItem(index: number) {
-    this.shoppingList.splice(index, 1);
+    this.apiService.deleteItem(index).subscribe((result: any) => {
+      console.log(result);
+      this.shoppingList.splice(index, 1);
+    })
   }
 
   toggleDone(event: any, index: number) {
     const isChecked = event.target.checked;
-    this.shoppingList[index].done = isChecked;
+    this.apiService.updateStatus(isChecked, index).subscribe((result: any) => {
+      console.log(result);
+      this.shoppingList[index].done = isChecked;
+    })
   }
 }
